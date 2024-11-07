25cf93f538a16442d1c22de339bdb0d8d2bd4fac
Kevin Mueller <kevin.mueller@tuta.com>
2024-11-07 10:17:32 +0100

    added Delete function with singleview

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import markets_view
+from .views import markets_view, market_single_view
 
 urlpatterns = [
-    path('', markets_view)
+    path('', markets_view),
+    path('<int:pk>/',market_single_view)
 ]
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -13,9 +13,23 @@ def markets_view(request):
         return Response(serializer.data)
 
     if request.method == 'POST':
-        serializer=MarketSerializer(data=request.data)
+        serializer = MarketSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+@api_view(['GET','DELETE'])
+def market_single_view(request,pk):
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data)
