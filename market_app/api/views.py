dae889092ca9532e961c19e2348b052b8de321bb
Kevin Mueller <kevin.mueller@tuta.com>
2024-11-07 10:47:13 +0100

    added put function to view and serializer

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -21,12 +21,21 @@ def markets_view(request):
             return Response(serializer.errors)
 
 
-@api_view(['GET','DELETE'])
-def market_single_view(request,pk):
+@api_view(['GET', 'DELETE', 'PUT'])
+def market_single_view(request, pk):
     if request.method == 'GET':
         market = Market.objects.get(pk=pk)
         serializer = MarketSerializer(market)
         return Response(serializer.data)
+
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
     if request.method == 'DELETE':
         market = Market.objects.get(pk=pk)
